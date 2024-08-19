636ae0d10412351de6ada82307cab0289624f418
Senorita15 <nicolenimpaye@gmail.com>
2024-08-19 14:13:32 -0400

    add activate field on User model

--- event/apps/authentication/migrations/0002_user_active.py
+++ event/apps/authentication/migrations/0002_user_active.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0001_initial'),
+        ("authentication", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='active',
+            model_name="user",
+            name="active",
             field=models.BooleanField(default=False),
         ),
     ]
