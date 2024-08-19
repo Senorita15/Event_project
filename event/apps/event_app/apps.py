12233db928504edf8ee7d24da490d02bca299a74
Senorita15 <nicolenimpaye@gmail.com>
2024-08-19 14:42:59 -0400

    changes made by pre-commit changes

--- event/apps/event_app/apps.py
+++ event/apps/event_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EventAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'event.apps.event_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "event.apps.event_app"
