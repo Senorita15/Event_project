e77fc349e11b1d67def62bf1ed3bafb5189120c7
Senorita15 <nicolenimpaye@gmail.com>
2024-08-19 13:47:52 -0400

    active form  and its access

--- event/apps/authentication/forms/__init__.py
+++ event/apps/authentication/forms/__init__.py
@@ -1,3 +1,4 @@
 from .user_creation_form import *
 from .user_update_form import *
 from .user_password_update_form import *
+from .user_activate_form import *
---
+++ event/apps/authentication/forms/user_activate_form.py
@@ -0,0 +1,13 @@
+from django import forms
+from event.apps.authentication.models import User
+from django import forms
+from django.utils import timezone
+
+
+class Useractivate(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "active"
+        ]
+     
