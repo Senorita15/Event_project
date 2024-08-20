48e5460e4f41c6aff6917f9e414ff6d7ae2b5b30
Senorita15 <nicolenimpaye@gmail.com>
2024-08-20 04:02:55 -0400

    event endpoints

--- event/apps/event_app/urls.py
+++ event/apps/event_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views.room import RoomViewSet, room_creation_form, room_edit_form
+from .views.event import EventViewSet, event_creation_form, event_edit_form
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"rooms", RoomViewSet, basename="room"),
-
+router.register(r"events", EventViewSet, basename="event"),
 
 urlpatterns = [
     # URLS ROOM
@@ -21,6 +22,26 @@ urlpatterns = [
         "room/delete/<int:pk>/",
         RoomViewSet.as_view({"get": "destroy"}),
         name="room-destroy",
+    ),
+    # URLS Events
+    path(
+        "event/status/<int:pk>/",
+        EventViewSet.as_view({"get": "update_evenement_status"}),
+        name="event-status",
+    ),
+    path("event/create_form/", event_creation_form.as_view(), name="event_create_room"),
+    path(
+        "event/edit_form/<int:pk>/", event_edit_form.as_view(), name="event-edit-form"
+    ),
+    path(
+        "event/update/<int:pk>/",
+        EventViewSet.as_view({"post": "update"}),
+        name="event-update",
+    ),
+    path(
+        "event/delete/<int:pk>/",
+        EventViewSet.as_view({"get": "destroy"}),
+        name="event-destroy",
     ),
     path("", include(router.urls)),
 ]
