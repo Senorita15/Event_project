ae64837b6ef20001d1b1c5920ad99412c45b8cab
Senorita15 <nicolenimpaye@gmail.com>
2024-08-20 02:03:38 -0400

    reservation serializer (changes made pre-commit)

--- event/apps/event_app/serializers/reservation.py
+++ event/apps/event_app/serializers/reservation.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from event_app.models.reservation import Reservation
 
-class Reservationserializer(serializers.ModelSerializer):
 
+class Reservationserializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = [
-            'last_name',
-            'first_name',
-            'phone_number',
-            'email',
-            'ticket_event',
-            'ticket_number',
-            'validated',
-            'created_at'
+            "last_name",
+            "first_name",
+            "phone_number",
+            "email",
+            "ticket_event",
+            "ticket_number",
+            "validated",
+            "created_at",
         ]
